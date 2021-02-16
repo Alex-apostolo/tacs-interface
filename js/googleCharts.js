e656bcec831f2f77e72a7bef8053e82bb0865b95
Alex-apostolo <alex-apostolo@hotmail.com>
2021-02-16 17:56:06 +0200

    Created custom element for the charts

--- js/googleCharts.js
+++ js/googleCharts.js
@@ -1,85 +1,70 @@
-const generalContainer = document.getElementById('general-container');
-const comparissonContainer = document.getElementById('comparisson-container');
-
-// Create container
-const pieContainer = document.createElement('div');
-pieContainer.classList.add('pie-container');
-// Create pie chart
-const pieChart = document.createElement('div');
-pieChart.id = 'pie-chart0';
-pieChart.classList.add('pie-chart');
-// Create minus button
-const minusBtn = document.createElement('div');
-minusBtn.classList.add('minus-btn');
-// Append to container
-pieContainer.append(pieChart);
-pieContainer.append(minusBtn);
-
 class TACSChart extends HTMLElement {
     constructor() {
         super();
+        this.innerHTML = `
+            <div class="pie-chart"></div>
+            <div class="minus-btn"></div>
+        `
     }
-}
 
-//Append to general container
-generalContainer.append(pieContainer);
+    connectedCallback() {
+        // Load the Visualization API and the piechart package.
+        google.load('visualization', '1.0', { 'packages': ['corechart'] });
 
-// Load the Visualization API and the piechart package.
-google.load('visualization', '1.0', { 'packages': ['corechart'] });
+        // Set a callback to run when the Google Visualization API is loaded.
+        google.setOnLoadCallback(this.drawChart.bind(this));
+    }
 
-// Set a callback to run when the Google Visualization API is loaded.
-google.setOnLoadCallback(drawChart);
+    // Callback that creates and populates a data table,
+    // instantiates the pie chart, passes in the data and
+    // draws it.
+    drawChart() {
+        console.log('cool');
+        console.log(this);
+        // Create the data table.
+        let data = new google.visualization.DataTable();
+        data.addColumn('string', 'Topping');
+        data.addColumn('number', 'Slices');
+        data.addRows([
+            ['Mushrooms', 3],
+            ['Onions', 1],
+            ['Olives', 1],
+            ['Zucchini', 1],
+            ['Pepperoni', 2]
+        ]);
 
-// Callback that creates and populates a data table,
-// instantiates the pie chart, passes in the data and
-// draws it.
-function drawChart() {
-
-    // Create the data table.
-    let data = new google.visualization.DataTable();
-    data.addColumn('string', 'Topping');
-    data.addColumn('number', 'Slices');
-    data.addRows([
-        ['Mushrooms', 3],
-        ['Onions', 1],
-        ['Olives', 1],
-        ['Zucchini', 1],
-        ['Pepperoni', 2]
-    ]);
-
-    // Set chart options
-    let options = {
-        width: 400,
-        height: 300,
-        backgroundColor: '#1f2761',
-        chartArea: { width: '100%', height: '80%' },
-        legend: {
-            position: 'bottom',
-            textStyle: {
-                color: 'whitesmoke',
-                fontName: 'EB Garamond'
-            }
-        },
-        hAxis: {
-            textStyle: {
+        // Set chart options
+        let options = {
+            width: 400,
+            height: 300,
+            backgroundColor: '#1f2761',
+            chartArea: { width: '100%', height: '80%' },
+            legend: {
+                position: 'bottom',
+                textStyle: {
+                    color: 'whitesmoke',
+                    fontName: 'EB Garamond'
+                }
+            },
+            hAxis: {
+                textStyle: {
+                    color: 'whitesmoke',
+                    fontName: 'EB Garamond',
+                    fontSize: 13
+                }
+            },
+            titleTextStyle: {
                 color: 'whitesmoke',
                 fontName: 'EB Garamond',
-                fontSize: 13
+                fontSize: 20
             }
-        },
-        titleTextStyle: {
-            color: 'whitesmoke',
-            fontName: 'EB Garamond',
-            fontSize: 20
-        }
-    };
-    options.title = 'Hello';
+        };
+        options.title = 'Hello';
 
-    // Instantiate and draw our chart, passing in some options.
-    let chart = new google.visualization.PieChart(pieChart);
-    chart.draw(data, options);
+        // Instantiate and draw our chart, passing in some options.
+        let chart = new google.visualization.PieChart(this.querySelector('.pie-chart'));
+        chart.draw(data, options);
+    }
+
 }
-
-customElements.define('Tacs-chart', TACSChart);
-
-export default TACSChart;
+customElements.define('tacs-chart', TACSChart);
