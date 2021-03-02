b4a141f849a0637cbf5a0031a0c4a848092369af
Alex-apostolo <alex-apostolo@hotmail.com>
2021-03-02 23:56:16 +0200

    Fallback commit for testing

--- js/controller.js
+++ js/controller.js
@@ -3,20 +3,32 @@ const backend = 'http://127.0.0.1:5000'
 // Add event listener for submitting the files
 const form = document.getElementById('form');
 form.addEventListener('submit', e => {
-    const selectedFiles = document.querySelectorAll('input');
     e.preventDefault();
+
     const formData = new FormData();
-    selectedFiles.forEach(selectedFiles => {
-        for (let i = 0; i < selectedFiles.files.length; i++) {
-            formData.append(selectedFiles.files[i].name, selectedFiles.files[i]);
-        }
+    const browseInput = document.querySelectorAll('browse-input');
+    const fileCount = [];
+
+    browseInput.forEach(selectedInput => {
+        const selectedFiles = selectedInput.querySelectorAll('input');
+        selectedFiles.forEach(selectedFiles => {
+            fileCount.push(selectedFiles.files.length);
+            for (let i = 0; i < selectedFiles.files.length; i++) {
+                // Add additional data to file to indicate that its in a specific group
+                formData.append('file', selectedFiles.files[i]);
+            }
+        });
     });
 
-    fetch(backend + '/tacs')
-    .then(result => result.text())
-    .then(json => console.log(json));
-    // Display the key/value pairs
-    for (var pair of formData.entries()) {
-        console.log(pair[0] + ', ' + pair[1]);
-    }
+    fetch(backend + '/tacs', {
+        method: 'POST',
+        body: formData
+    })
+        .then(response => response.json())
+        .then(object => {
+            // Iterate through all the files and combine the frequency counter
+            console.log(object);
+            // If more than one group is used, then calculate their general parts and compare
+            // Display first five in Specific field
+        });
 });
